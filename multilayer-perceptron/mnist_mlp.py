6441b946467d7b71a82f01bc6249553acf6b975c
Walter, Tobias <69347097+TobiWan123@users.noreply.github.com>
2024-05-22 20:30:59 +0200

    plots mnits

--- multilayer-perceptron/mnist_mlp.py
+++ multilayer-perceptron/mnist_mlp.py
@@ -15,49 +15,67 @@ y_train_encoded, y_test_encoded = np.zeros((y_train.size, 10)), np.zeros((y_test
 y_train_encoded[np.arange(y_train.size), y_train] = 1
 y_test_encoded[np.arange(y_test.size), y_test] = 1
 
+# Berechnung der Anzahl der Reihen (4 Reihen, da 12/3 = 4)
+rows = 4
+
 # Durchschnittsbilder für jede Klasse visualisieren
-fig, axes = plt.subplots(1, 10, figsize=(10, 5))
-for i in range(10):
-    mean_img = np.mean(x_train[y_train == i], axis=0)
-    axes[i].imshow(mean_img.reshape(28, 28), cmap='gray')
-    axes[i].set_title(str(i))
-    axes[i].axis('off')
+fig, axes = plt.subplots(rows, 3, figsize=(12, 12))
+for i in range(12):  # Gehe von 0 bis 11, für 12 Bilder
+    row = i // 3  # Bestimmt die Reihe
+    col = i % 3   # Bestimmt die Spalte
+    if i < 10:
+        mean_img = np.mean(x_train[y_train == i], axis=0)
+        ax = axes[row, col]
+        ax.imshow(mean_img.reshape(28, 28), cmap='gray')
+        ax.set_title(str(i))
+    else:
+        # wenn keine zusätzlichen Klassenbilder vorhanden sind
+        ax = axes[row, col]
+        ax.imshow(np.zeros((28, 28)), cmap='gray')  # Zeige ein leeres Bild
+        ax.set_title("Empty")
+    ax.axis('off')
+
+plt.subplots_adjust(hspace=0.1, wspace=0.1)
+plt.tight_layout(pad=2.0)
+
 # plt.show()
 
 # Netzwerk initialisieren (Normal)
 layers = [
-    k.DenseLayer(784, 256, acf=kh.sigmoid),
-    k.DenseLayer(256, 256, acf=kh.sigmoid),
-    k.DenseLayer(256, 10, acf=kh.softmax)
+    k.DenseLayer(784, 256, acf=kh.relu),
+    k.DenseLayer(256, 256, acf=kh.relu),
+    k.DenseLayer(256, 10, acf=kh.relu)
 ]
 
-mlp = k.MLP(*layers, cost=kh.nll)
+mlp = k.MLP(*layers, cost=kh.bce)
 
 # Training des Netzwerks
-k.train(mlp, x_train.T, y_train_encoded.T, epochs=500, lr=0.0001, batch_size=20)
+k.train(mlp, x_train.T, y_train_encoded.T, epochs=1000, lr=0.0001)
 
 # Netzwerk initialisieren (klein)
-layers_small = [
-    k.DenseLayer(784, 128, acf=kh.sigmoid),
-    k.DenseLayer(128, 64, acf=kh.sigmoid),
-    k.DenseLayer(64, 10, acf=kh.sigmoid)
+layers_big = [
+    k.DenseLayer(784, 128, acf=kh.relu),
+    k.DenseLayer(512, 256, acf=kh.relu),
+    k.DenseLayer(256, 128, acf=kh.relu),
+    k.DenseLayer(128, 64, acf=kh.relu),
+    k.DenseLayer(64, 10, acf=kh.relu)
 ]
 
-mlp_small = k.MLP(*layers_small)
+mlp_big = k.MLP(*layers_big, cost=kh.bce)
 
 # Training des Netzwerks
 # k.train(mlp_small, x_train.T, y_train_encoded.T, epochs=50, lr=0.00001, batch_size=50)
 
 # Netzwerk initialisieren (winzig)
 layers_smaller = [
-    k.DenseLayer(784, 64, acf=kh.sigmoid),
-    k.DenseLayer(64, 10, acf=kh.sigmoid)
+    k.DenseLayer(784, 64, acf=kh.relu),
+    k.DenseLayer(64, 10, acf=kh.relu)
 ]
 
-mlp_smaller = k.MLP(*layers_smaller, cost=kh.mse_cost)
+mlp_smaller = k.MLP(*layers_smaller, cost=kh.bce)
 
 # Training des Netzwerks
-# k.train(mlp_smaller, x_train.T, y_train_encoded.T, epochs=50, lr=0.00001, batch_size=50)
+k.train(mlp_smaller, x_train.T, y_train_encoded.T, epochs=1000, lr=0.01)
 
 predicted = mlp_smaller.predict(x_test.T)
 accuracy_score = k.accuracy(predicted[0], y_test_encoded.T)
